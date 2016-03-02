128ff4e5ae7b95559c3abf2f8538950792595ece
Stuti Agrawal <stuti.agrawal@gmail.com>
2016-03-02 22:40:52 +0000

    fix(slurm): adding ability to use multiple gateways to ceph

--- slurm/template.sh
+++ slurm/template.sh
@@ -15,10 +15,21 @@ username="XX_username_XX"
 password="XX_password_XX"
 repository="git@github.com:NCI-GDC/varscan-cwl.git"
 s3dir="s3://washu_varscan_variant/"
+access_key=$access_key
+secret_key=$secret_key
+host_base="XX_HOST_BASE_XX"
+
 wkdir=`mktemp -d -p /mnt/SCRATCH` 
 cd $wkdir 
+
+s3cfg=${wkdir}/s3cfg
+echo "[default]" > $s3cfg
+echo "access_key = $access_key" >> $s3cfg
+echo "secret_key = $secret_key" >> $s3cfg
+echo "host_base = $host_base" >> $s3cfg
+
 sudo git clone -b feat/slurm $repository  
 sudo chown ubuntu:ubuntu varscan-cwl 
 cwl=$wkdir/varscan-cwl/tools/varscan-tool.cwl.yaml
-/home/ubuntu/.virtualenvs/p2/bin/python $wkdir/varscan-cwl/slurm/run_cwl.py --ref $ref --refindex $refindex --normal $normal --tumor $tumor --normal_id $normal_id --tumor_id $tumor_id --case_id $case_id --username $username --password $password --basedir $basedir --cwl $cwl --s3dir $s3dir
+/home/ubuntu/.virtualenvs/p2/bin/python $wkdir/varscan-cwl/slurm/run_cwl.py --ref $ref --refindex $refindex --normal $normal --tumor $tumor --normal_id $normal_id --tumor_id $tumor_id --case_id $case_id --username $username --password $password --basedir $basedir --cwl $cwl --s3dir $s3dir -s3ceph $s3cfg
 sudo rm -rf $wkdir
